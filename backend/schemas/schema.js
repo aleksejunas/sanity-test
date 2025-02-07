0c5b75bed13d4dbed72b77a1947de50a2f660b38
aleksejunas <photo@seventyseven.no>
2025-02-07 12:57:34 +0100

    some minor stuff

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -1,5 +1,5 @@
-// import createSchema from 'part:@sanity/base/schema-creator'
-// import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
 import homepage from './homepage'
 import post from './post'
 
